b879402bcf3288fae00f9b3119a0bd2a409effbe
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2021-07-20 18:40:30 +0600

    Create DoublePrecision.cpp

---
+++ DoublePrecision.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    double out = (1.0*2)/(1.0*4);
+    cout << fixed;
+    cout << setprecision(12) << out << endl;
+    /// Output : 0.500000000000
+}
