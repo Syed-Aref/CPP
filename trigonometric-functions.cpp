b40e7c57defe9f0a4f624c4d0cebb205b268effb
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2021-10-27 00:12:35 +0600

    Add files via upload

---
+++ trigonometric-functions.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.14159265
+const int nmax = 1e5;
+
+bool w[nmax+5] , s1[nmax+5];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    cout << sin(30) << endl;
+    cout << sin(30*(PI/180)) << endl;
+}
+
+
