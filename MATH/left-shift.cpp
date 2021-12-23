25833db326a3bb2e9c774fad62c75d5764993c59
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2021-12-23 21:48:17 +0600

    Create left-shift.cpp

---
+++ MATH/left-shift.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+
+
+    int n;
+    /// Indexing: ...,4,3,2,1,0
+    n = 0;
+    cout << (1<<n) << endl; /// Binary: 0001
+
+    n = 1;
+    cout << (1<<n) << endl; /// Binary: 0010
+
+    n = 2;
+    cout << (1<<n) << endl; /// Binary: 0100
+
+    n = 3;
+    cout << (1<<n) << endl; /// Binary: 1000
+
+
+}
