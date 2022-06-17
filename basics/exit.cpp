d099ded4d8ac4b3450f92f1d644b6f0f94cc0008
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2022-06-17 10:33:00 +0600

    Create exit.cpp

---
+++ basics/exit.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    if(1){
+        cout << "bye" << endl;
+        return 0;
+    }
+    cout << "here?" << endl;
+
+
+}
