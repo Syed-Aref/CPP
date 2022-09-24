ac65aa25676a6edd3318ed4db3a65db6b0b29c11
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2022-09-24 21:33:12 +0600

    Create ordered_set_for_integers.cpp

---
+++ DS/Ordered Set/ordered_set_for_integers.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+///For ordered set of integers
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+
+typedef tree<int , null_type , less<int> , rb_tree_tag , tree_order_statistics_node_update> ordered_set;
+
+int main()
+{
+    ordered_set p;
+
+    p.insert(22);
+    p.insert(1);
+    p.insert(20);
+    p.insert(22);
+
+    ///No duplicate elements
+    cout << p.size() << endl;
+
+    cout << "========================================" << endl;
+
+    ///find_by_order(i): i-th element in the set(logn).
+    for(int i = 0;i<p.size();i++){
+        cout << *( p.find_by_order(i) ) << endl;
+    }
+
+    cout << "========================================" << endl;
+
+    ///order_of_key(x) : Number of items strictly smaller than x(logn).
+    cout << p.order_of_key(22) << endl;
+    cout << p.order_of_key(23) << endl;
+
+    cout << "========================================" << endl;
+
+    ///Clearing entire list
+    p.clear();
+    cout << p.size() << endl;
+
+    return 0;
+}
