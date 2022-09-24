3e5904701ab49e72acb5d6cefd11d1af9e767033
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2022-09-25 01:46:42 +0600

    Update String_methods.cpp

--- String_Folder/String_methods.cpp
+++ String_Folder/String_methods.cpp
@@ -11,18 +11,24 @@ typedef long long int lli;
 #define line_ "\n"
 
 /// string breaking
-vector<string> stringBreaker(string& line1,char& c) {
-
-    vector <string> tokens;
-    stringstream check(line1);
-    string intermediate;
-    while(getline(check, intermediate, c))
+vector<string> splitString(string str,char breaker)
+{
+    vector<string> ret;
+    string s = "";
+    for (auto x : str)
     {
-        tokens.push_back(intermediate);
+        if (x == breaker)
+        {
+            if(s.length() > 0)ret.push_back(s);
+            s = "";
+        }
+        else {
+            s = s + x;
+        }
     }
-    return tokens;
-
+    return ret;
 }
+
 
 /// string to int
 int stringToInt(string sn) {
@@ -54,32 +60,4 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
-    int on;
-    cout << "press 0 to exit" << endl;
-    cin >> on;
-    while(on) {
-
-        string s;
-        getline(cin , s);
-        char c = ' ';
-        auto tokens = stringBreaker(s,c);
-        for(auto i : tokens) {
-            cout << i << endl;
-        }
-        cout << "press 0 to exit" << endl;
-        cin >> on;
-    }
-
-    cout << "press 0 to exit" << endl;
-    cin >> on;
-    while(on){
-        string sn;
-        cin >> sn;
-        auto x = stringToInt(sn);
-        cout << x << endl;
-        cout << x*10 << endl;
-        cout << "press 0 to exit" << endl;
-        cin >> on;
-    }
 }
