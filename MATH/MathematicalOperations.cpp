6852dea853385a1f5b6679d719da8fcfc5a03251
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2022-09-25 21:19:43 +0600

    Create MathematicalOperations.cpp

---
+++ MATH/MathematicalOperations.cpp
@@ -0,0 +1,11 @@
+/**
+
+
+--> double is more recommended than float
+--> #include <cmath>
+--> round(num),ceil(num),floor(num) taks double(data type) input and returns integer
+--> sqrt(num),cbrt(num) taks double(data type) input and returns double
+--> fmax(n1,n2) : n1,n2 can be any data type and retuns the largest
+--> fmin(n1,n2) : n1,n2 can be any data type and retuns the smallest
+
+**/
