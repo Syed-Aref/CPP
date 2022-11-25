c2f0c1997747420b8d8375a0e612bedb0159f17c
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2022-11-25 11:38:25 +0600

    Create data_type_size.cpp

---
+++ Basics/data_type_size.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+
+    cout << "Size of bool            : " << sizeof(bool) << endl;           ///1 
+  
+    cout << "Size of char            : " << sizeof(char) << endl;           ///1
+  
+    cout << "Size of int             : " << sizeof(int) << endl;            ///4
+    cout << "Size of long long int   : " << sizeof(long long int) << endl;  ///8
+  
+    cout << "Size of float           : " << sizeof(float) << endl;          ///4
+    cout << "Size of double          : " << sizeof(double) << endl;         ///8
+
+    return 0;
+}
