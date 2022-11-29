e0592d8593078d025208e58bbcff68c9dd8b5e8f
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2022-11-29 13:14:32 +0600

    Update and rename Pointers2.cpp to Pointers_and_Reference.cpp

---
+++ Pointers_Folder/Pointers_and_Reference.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+
+void f1(int x) {
+    cout << "Memory address of x = " << &x << endl;
+    x = x + 2;
+}
+
+void f2(int* x) {
+    cout << "Memory address of x = " << x << endl;
+    *x = *x + 4;
+}
+
+void f3(int& x) {
+    cout << "Memory address of x = " << &x << endl;
+    x = x + 8;
+}
+
+int main()
+{
+
+    ///Pointer: memory address of a variable
+    ///Pointer referencing: assigning memory address to a pointer variable
+    int a = 34;
+    int *pA = &a;
+    cout << "Value : " << a << endl;
+    cout << "Memory : " << pA << endl;
+
+    double d = 69.69;
+    double *pD = &d;
+    cout << "Value : " << d << endl;
+    cout << "Memory : " << pD << endl;
+
+
+    ///Pointer de-referencing: extracting value from memory address
+    cout << "a = "<<*pA << endl;
+    cout << "d = " << *pD << endl;
+
+    cout << "========================================================================================" << endl;
+
+    ///Reference: a variable(new) that refers to another existing variable.
+    ///So, new variable will refer to the same object that existing variable refers.
+    ///Both variables will have same memory address.
+    string str1 = "Wew";
+    string& str2 = str1;
+    cout << str1 << endl;
+    cout << str2 << endl;
+
+    str1 = "eqeq";
+    cout << str1 << endl;
+    cout << str2 << endl;
+
+    str1 = "whfhiihfiw";
+    cout << str1 << endl;
+    cout << str2 << endl;
+
+    cout << "========================================================================================" << endl;
+
+    ///Passing copy of the value of a variable as a parameter(pass by value)
+    int a1 = 10;
+    cout << "Memory address of a1 = " << &a1 << endl;
+    cout << "a1 = " << a1 << endl;
+    f1(a);
+    cout << "a1 = " << a1 << endl;
+
+    cout << "========================================================================================" << endl;
+
+    ///Passing memory address of a variable as a paramter
+    int a2 = 10;
+    cout << "Memory address of a2 = " << &a2 << endl;
+    cout << "a2 = " << a2 << endl;
+    f2(&a2);
+    cout << "a2 = " << a2 << endl;
+
+    cout << "========================================================================================" << endl;
+
+    ///Passing reference of a variable as a parameter(pass by reference)
+    int a3 = 10;
+    cout << "Memory address of a3 = " << &a3 << endl;
+    cout << "a3 = " << a3 << endl;
+    f3(a3);
+    cout << "a3 = " << a3 << endl;
+
+}
