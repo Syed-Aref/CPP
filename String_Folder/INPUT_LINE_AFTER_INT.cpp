bbd3991b0441d8648283a52b4f9e0ae142c94f65
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2022-11-30 11:19:12 +0600

    Add files via upload

---
+++ String_Folder/INPUT_LINE_AFTER_INT.cpp
@@ -0,0 +1,40 @@
+///Bismillahir Rahmanir Rahim
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
+
+const int nmax = 1E5 + 5;
+const lli mod = 1E9 + 7;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    int t = 1;
+    ///cin >> t;
+
+    while(t-->0){
+        int n;
+        string s;
+
+        cin >> n;
+        cin.ignore();
+        getline(cin , s);
+
+        cout << n << endl;
+        cout << s << endl;
+    }
+
+}
