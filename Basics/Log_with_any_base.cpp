76d499c22daf0444c9b3770a2571c668864da0d5
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2023-01-26 13:27:58 +0600

    Create Log_with_any_base.cpp

---
+++ Basics/Log_with_any_base.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+int main() {
+
+
+    int x = 556, base = 2;
+    cout << log(x) << endl;
+    cout << log(x) / log(base) << endl;
+
+    x = 256, base = 2;
+    cout << log(x) << endl;
+    cout << log(x) / log(base) << endl;
+}
