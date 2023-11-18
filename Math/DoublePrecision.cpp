f09a3687d7c43b74caa8e35baa9cb292fc996528
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2023-11-19 03:55:55 +0600

    Update DoublePrecision.cpp

--- Math/DoublePrecision.cpp
+++ Math/DoublePrecision.cpp
@@ -10,5 +10,11 @@ int main()
     double out = (1.0*2)/(1.0*4);
     cout << fixed;
     cout << setprecision(12) << out << endl;
-    /// Output : 0.500000000000
+    /// Output : 0.500000000000 
+
+    /// Precision without rounding:
+    out = 16.27777;
+    out = floor(out*10000) / 10000.0;
+    cout << out << endl; 
+    /// Output : 16.2777
 }
