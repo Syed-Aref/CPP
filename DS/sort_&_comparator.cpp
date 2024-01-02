6838830b0d5befee33ef1e26a51f4e4de2d840d7
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-01-02 13:14:32 +0600

    Update and rename sort and sorting comparator(vector) to sort_&_comparator.cpp

--- DS/sort_&_comparator.cpp
+++ DS/sort_&_comparator.cpp
@@ -2,8 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool sort_func1(int x, int y) // a1,a2
-{
+bool sort_func1(int x, int y) // a1,a2{
     return x > y; //a1 > a2
 }
 bool sort_func2(int x, int y) // a1,a2
@@ -12,29 +11,28 @@ bool sort_func2(int x, int y) // a1,a2
 }
 int main()
 {
-    vector< int > v = { 23,67,56,89,67,34 };
     // Built-in sort function that sorts in non-decreasing order
+    vector< int > v = { 23,67,56,89,67,34 };
     sort(v.begin(), v.end());
     for (int i = 0; i < v.size(); i++)
         cout << v[i] << "\n";
     cout << "---------------------" << "\n";
 
-    vector< int > v1 = { 23,67,56,89,67,34 };
     // Sort in non-increasing order using comparator
+    vector< int > v1 = { 23,67,56,89,67,34 };
     sort(v1.begin(), v1.end(), sort_func1);
     for (int i = 0; i < v1.size(); i++)
         cout << v1[i] << "\n";
     cout << "---------------------" << "\n";
 
-    vector< int > v2 = { 23,67,56,89,67,34 };
     // Sort in non-decreasing order using comparator
+    vector< int > v2 = { 23,67,56,89,67,34 };
     sort(v2.begin(), v2.end(), sort_func2);
     for (int i = 0; i < v2.size(); i++)
         cout << v2[i] << "\n";
     cout << "---------------------" << "\n";
     
     /**
-    
     Non-decreasing:
     func1(a1,a2){
         returns a1 < a2;
@@ -44,11 +42,11 @@ int main()
     func2(a1,a2){
         returns a1 > a2;
     }
-    
     **/
     
-    //Built-in method for non-increasing ordered sort
-    sort( v2.begin() , v2.end() , greater<int>() );
+    /// Built-in method for non-increasing ordered sort
     /// greater< any_accepted_data_type >() 
+    sort( v2.begin() , v2.end() , greater<int>() );
+    
 }
 
