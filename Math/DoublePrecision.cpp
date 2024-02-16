2bff10e36ce84d40c97c71b5a37b553832872b08
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 16:10:56 +0600

    Update DoublePrecision.cpp

--- Math/DoublePrecision.cpp
+++ Math/DoublePrecision.cpp
@@ -1,8 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+typedef long long int lli;
+
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
