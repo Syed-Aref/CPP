4cc9959ca906dd53de360b22dd95fe129c0c2c1d
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 16:11:48 +0600

    Update and rename left-shift.cpp to left_shift.cpp

--- Math/left_shift.cpp
+++ Math/left_shift.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+typedef long long int lli;
+
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
 
 int main() {
     ios::sync_with_stdio(false);
