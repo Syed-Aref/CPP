34ea51d6012ff7d1b573fcd37c51ff6ec1bc920d
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 15:57:24 +0600

    Update typedef_examples.cpp

--- Basics/typedef_examples.cpp
+++ Basics/typedef_examples.cpp
@@ -10,8 +10,14 @@ Structure: typedef [data type] data_type_alternate name
 typedef long long int lli;
 typedef pair<int,int> pii;
 
-int main()
-{
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
