4f0297b1e7b2913003013698e195f929b5a2090c
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 16:51:23 +0600

    Update and rename Struct to Struct.cpp

--- OOP/Struct.cpp
+++ OOP/Struct.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+typedef long long int lli;
+
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
+
 struct Student {
 	//age first_name last_name standard
 	int age;
@@ -8,8 +17,12 @@ struct Student {
 	string last_name;
 	int standard;
 };
-int main() 
-{
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
     Student st;
     
     cin >> st.age >> st.first_name >> st.last_name >> st.standard;
