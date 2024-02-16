db8286b974d7c897dba143ba9991f5acfe190f80
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 15:55:39 +0600

    Update Log_with_any_base.cpp

--- Basics/Log_with_any_base.cpp
+++ Basics/Log_with_any_base.cpp
@@ -1,12 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long int lli;
 
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
 
 
 
 int main() {
-
+ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     int x = 556, base = 2;
     cout << log(x) << endl;
