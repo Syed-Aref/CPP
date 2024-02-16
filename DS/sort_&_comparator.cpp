c1fc9cef42f72a10e23a959a83ff9908a0835c4e
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 16:06:50 +0600

    Update sort_&_comparator.cpp

--- DS/sort_&_comparator.cpp
+++ DS/sort_&_comparator.cpp
@@ -2,27 +2,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool sort_func1(int x, int y) // a1,a2{
-    return x > y; //a1 > a2
+typedef long long int lli;
+
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
+
+bool sort_func1(int x, int y){ /// a1,a2
+    return x > y; ///a1 > a2
 }
-bool sort_func2(int x, int y) // a1,a2
-{
-    return x < y; // a1 < a2
+bool sort_func2(int x, int y){ /// a1,a2
+    return x < y; /// a1 < a2
 }
-int main()
-{
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     // Built-in sort function that sorts in non-decreasing order
     vector< int > v = { 23,67,56,89,67,34 };
     sort(v.begin(), v.end());
-    for (int i = 0; i < v.size(); i++)
-        cout << v[i] << "\n";
+    for (int i = 0; i < v.size(); i++) cout << v[i] << "\n";
     cout << "---------------------" << "\n";
 
     // Sort in non-increasing order using comparator
     vector< int > v1 = { 23,67,56,89,67,34 };
     sort(v1.begin(), v1.end(), sort_func1);
-    for (int i = 0; i < v1.size(); i++)
-        cout << v1[i] << "\n";
+    for (int i = 0; i < v1.size(); i++) cout << v1[i] << "\n";
     cout << "---------------------" << "\n";
 
     // Sort in non-decreasing order using comparator
@@ -34,12 +43,12 @@ int main()
     
     /**
     Non-decreasing:
-    func1(a1,a2){
+    func1(a1,a2) {
         returns a1 < a2;
     }
     
     Non-increasing:
-    func2(a1,a2){
+    func2(a1,a2) {
         returns a1 > a2;
     }
     **/
