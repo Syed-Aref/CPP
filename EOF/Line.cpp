234d8e5e2084a6deec127dd3602ebc4d0e731117
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 16:10:23 +0600

    Update and rename Line to Line.cpp

--- EOF/Line.cpp
+++ EOF/Line.cpp
@@ -1,17 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+typedef long long int lli;
 
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     string s = "";
     string line;
     vector<string> vstr;
 
     //Inputting lines until eof and string them in s
-    while (getline(cin, line))
-    {
+    while (getline(cin, line)) {
     	s = s + " " + line;
     	// storing all inputed characters in s with an addition ' '
     	// before every inputs
@@ -29,16 +38,14 @@ int main()
     // string s to char[] str
     char *str = new char[s.length()];
     int sz = s.length();
-    for (int i = 0; i < sz; i++)
-    {
+    for (int i = 0; i < sz; i++) {
         str[i] = s[i];
     }
 
     //printing all single string elements
     cout << "All single elements" << endl;
     char* token = strtok(str, " ");
-    while (token != NULL)
-    {
+    while (token != NULL) {
         printf("%s\n", token);
         token = strtok(NULL, " ");
     }
