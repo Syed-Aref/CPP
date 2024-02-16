77e1e4ad6d2f0ee5aa59a23aa848e2e919b2d5f6
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 15:56:56 +0600

    Update data_type_size.cpp

--- Basics/data_type_size.cpp
+++ Basics/data_type_size.cpp
@@ -1,10 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long int lli;
 
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
 
 int main() {
-
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     cout << "Size of bool            : " << sizeof(bool) << endl;           ///1 
   
     cout << "Size of char            : " << sizeof(char) << endl;           ///1
