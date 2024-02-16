148a4d0a8d2fe8631ef6ba9f5e7268496bf7229b
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 16:12:12 +0600

    Update trigonometric-functions.cpp

--- Math/trigonometric-functions.cpp
+++ Math/trigonometric-functions.cpp
@@ -1,12 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+typedef long long int lli;
+
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
 #define PI 3.14159265
+
 const int nmax = 1e5;
 
 bool w[nmax+5] , s1[nmax+5];
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
