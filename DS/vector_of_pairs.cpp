b73cd392f188ec7b34524d1dbca0571ecc472de5
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 16:08:28 +0600

    Update vector_of_pairs.cpp

--- DS/vector_of_pairs.cpp
+++ DS/vector_of_pairs.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+
+typedef long long int lli;
+
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
+
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     //initialize:
     vector< std::pair< int, string > > pv;
     
@@ -12,8 +25,7 @@ int main() {
     pv.push_back(std::pair< int, string >(10, "Messi"));
     
     //Traversing
-    for ( auto p : pv)
-    {
+    for ( auto p : pv) {
         cout << p.second<<" will wear "<< p.first<<"\n";
     }
 }
