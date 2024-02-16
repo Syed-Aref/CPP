e1b46e33cefa88bb51eff50fceb56167c46fd2b6
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 16:05:23 +0600

    Update pair.cpp

--- DS/pair.cpp
+++ DS/pair.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+typedef long long int lli;
 
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     ///Initialize a pair
     pair<int, int> p(10, 20);
     pair<char, int> ci = make_pair('a', 1);
