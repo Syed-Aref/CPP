9fefbbeda8b07f4cb2e43a45fc75b95b267efa2e
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 16:02:54 +0600

    Update max_priority_queue.cpp

--- DS/max_priority_queue.cpp
+++ DS/max_priority_queue.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long int lli;
+
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
+
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     /**Max priority queue**/
     priority_queue<int> q;
