31bb11327c3e7e5897aa7c0b86bd452ce0014cce
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2024-02-16 16:04:09 +0600

    Update min_priority_queue.cpp

--- DS/min_priority_queue.cpp
+++ DS/min_priority_queue.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+typedef long long int lli;
 
+#define mpr make_pair
+#define pbk push_back
+#define psh push
+#define fir first
+#define sec second
+#define line "\n"
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     /**Min priority queue**/
     priority_queue<int , vector<int> , greater<int>> q;
 
